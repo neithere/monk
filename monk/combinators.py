cd08cc4a71dbed96caa8c3c1575b09d52a6e82a7
Andy Mikhaylenko <neithere@gmail.com>
2014-08-22 02:51:16 +0600

    Add draft for requirement combinators and stuff

---
+++ monk/combinators.py
@@ -0,0 +1,83 @@
+# coding: utf-8
+#
+#    Monk is an unobtrusive data modeling, manipulation and validation library.
+#    Copyright © 2011—2013  Andrey Mikhaylenko
+#
+#    This file is part of Monk.
+#
+#    Monk is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Monk is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with Monk.  If not, see <http://gnu.org/licenses/>.
+"""
+Value Specification Combinators
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+__all__ = [
+    'BaseCombinator',
+    'All',
+    'Any',
+]
+
+
+from .errors import CombinedValidationError, ValidationError
+
+
+class BaseCombinator:
+    def __init__(self, specs, default=None):
+        assert specs
+        self._specs = specs
+        self._default = default
+
+    def __call__(self, value):
+        errors = []
+        for spec in self._specs:
+            # TODO: group errors by exception type
+            if spec.is_recursive and errors:
+                # Don't collect nested errors if we already have one here.
+                # Another optimized strategy would be to fail early instead of
+                # trying to collect all exceptions for the node.
+                continue
+            try:
+                spec(value)
+            except ValidationError as e:
+                errors.append(e)
+        if not self.can_tolerate(errors):
+            raise CombinedValidationError('invalid value {!r} ({})'.format(
+                value, '; '.join(map(str, errors))))
+
+
+    def __repr__(self):
+        return '<{} [{}]>'.format(
+            self.__class__.__name__,
+            ', '.join(map(str, self._specs)))
+
+    @property
+    def default(self):
+        # TODO: default value gathering strategy:
+        # 1. explicitly defined; if none--
+        # 2. poll the specs; if gathered exactly one unique value -- use it
+        return self._default
+
+    def can_tolerate(self, errors):
+        raise NotImplementedError
+
+
+class All(BaseCombinator):
+    def can_tolerate(self, errors):
+        if not errors:
+            return True
+
+
+class Any(BaseCombinator):
+    def can_tolerate(self, errors):
+        if len(errors) < len(self._specs):
+            return True
---
+++ monk/reqs.py
@@ -0,0 +1,127 @@
+# coding: utf-8
+#
+#    Monk is an unobtrusive data modeling, manipulation and validation library.
+#    Copyright © 2011—2013  Andrey Mikhaylenko
+#
+#    This file is part of Monk.
+#
+#    Monk is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Monk is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with Monk.  If not, see <http://gnu.org/licenses/>.
+"""
+Value Specification Classes
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+__all__ = [
+    'BaseRequirement',
+    'IsA',
+    'Equals',
+    'CanBeNone',
+    'Required',
+    'Optional',
+    'DictContains',
+    'ListContains',
+]
+
+from functools import partial
+
+from .errors import ValidationError, MissingValue
+
+
+class BaseRequirement:
+    # a hint for combinators, see their code
+    is_recursive = False
+    implies = ()
+
+    def __call__(self, value):
+        for implied in self.implies:
+            implied(value)
+        self.check(value)
+
+    def _represent(self):
+        return self.__dict__
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, self._represent())
+
+
+class IsA(BaseRequirement):
+    def __init__(self, expected_type):
+        self.expected_type = expected_type
+
+    def check(self, value):
+        if not isinstance(value, self.expected_type):
+            raise ValidationError('must be {}'.format(self.expected_type.__name__))
+
+    def _represent(self):
+        return self.expected_type.__name__
+
+
+class Equals(BaseRequirement):
+    def __init__(self, expected_value):
+        self.expected_value = expected_value
+
+    def check(self, value):
+        if self.expected_value != value:
+            raise ValidationError('!= {}' .format(self.expected_value))
+
+    def _represent(self):
+        return repr(self.expected_value)
+
+
+class CanBeNone(BaseRequirement):
+    def __init__(self, is_allowed):
+        self.is_allowed = is_allowed
+
+    def check(self, value):
+        if value is None and not self.is_allowed:
+            raise MissingValue('must be defined')
+
+    def _represent(self):
+        return self.is_allowed
+
+Required = partial(CanBeNone(False))
+Optional = partial(CanBeNone(True))
+
+
+class ListContains(BaseRequirement):
+    is_recursive = True
+    implies = [IsA(list), CanBeNone(False)]
+
+    def __init__(self, req):
+        self.nested_req = req
+
+    def check(self, value):
+        for nested_value in value:
+            self.nested_req(nested_value)
+
+    def _represent(self):
+        return repr(self.nested_req)
+
+
+class DictContains(BaseRequirement):
+    is_recursive = True
+    implies = [IsA(dict), CanBeNone(False)]
+
+    def __init__(self, key, req):
+        self.key = key
+        self.nested_req = req
+
+    def check(self, value):
+        nested_value = value[self.key]
+        self.nested_req(nested_value)
+
+
+#@requirement(implies=[IsA(dict)], is_recursive=True, vars=['key', 'req'])
+#def dict_contains(ctx, value):
+#    nested_value = value[ctx['key']]
+#    ctx['req'](nested_value)
